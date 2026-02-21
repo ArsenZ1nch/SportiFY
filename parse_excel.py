481bb66e3d649bfd1e4de5a595ed4c5f239b6bb7
ArsenZ1nch <arsen.zinch@gmail.com>
2026-02-21 14:01:52 +0100

    idk digga

--- parse_excel.py
+++ parse_excel.py
@@ -1,1 +1,13 @@
+import openpyxl
 
+
+if __name__ == "__main__":
+    from openpyxl import Workbook
+
+    EXCEL_PATH = "test/test.xlsx"
+    workbook = openpyxl.load_workbook(EXCEL_PATH)
+    worksheet = workbook.active
+
+    for column in worksheet.columns:
+        for cell in column:
+            print(cell, cell.value)
